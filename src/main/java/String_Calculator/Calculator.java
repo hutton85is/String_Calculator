bf92891125a27ee891cd11d3aa535fc15345bbe9
hutton85is <hutton85is@yahoo.com>
2016-10-13 21:26:25 +0000

    added function to turn str to integer array, tested for empty value and one number

--- src/main/java/String_Calculator/Calculator.java
+++ src/main/java/String_Calculator/Calculator.java
@@ -7,7 +7,29 @@ public class Calculator
 	//recive integer array and return their sum
 	public static int sum(int ... a)
 	{
-		return 6;
+		int result = 0;
+		//summing all elements of an array
+		for (int i = 0; i < a.length; i++)
+		{
+			result += a[i];
+		}
+		return result;
+	}
+
+	//split string into integer array
+	public static int[] strtoint(String s)
+	{
+
+		int[] arr = new int[1];
+		if (s == "")
+		{
+			arr[0] = 0;
+		}
+		else
+		{
+			arr[0] = Integer.parseInt(s);
+		}
+		return arr;
 	}
 
 	//recieve numbers as text, and return their sum
--- src/test/java/String_Calculator/CalculatorTest.java
+++ src/test/java/String_Calculator/CalculatorTest.java
@@ -1,5 +1,7 @@
 package String_Calculator;
 
+import java.util.*;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -7,21 +9,51 @@ public class CalculatorTest {
 
 	//Test input of an empty string
 	@Test
-	public void test_EmptyString()
+	public void test_add_EmptyString()
 	{
 		assertEquals(0, Calculator.add(""));
 	}
 
+	//Test input of a single value
 	@Test
-	public void test_one_value()
+	public void test_add_one_value()
 	{
 		assertEquals(1,Calculator.add("1"));
 	}
 
+	//Test obtaining result of addition of 
+	//a single element in an array
 	@Test
-	public void test_multiple_numbers()
+	public void test_sum_zero()
+	{
+		int[] a = {0};
+		assertEquals(0,Calculator.sum(a));
+	}
+
+	//Test summing three elements of an array
+	@Test
+	public void test_sum_multiple_numbers()
 	{
 		int[] a = {1,2,3};
 		assertEquals(6,Calculator.sum(a));
 	}
+
+	//turn empty string into an integer array
+	@Test
+	public void test_strtoint_emptyvalue()
+	{
+		int[] arr = new int[1];
+		arr[0] = 0;
+		assertArrayEquals(arr,Calculator.strtoint(""));
+	}
+
+	//Turn string with one number to array
+	@Test
+	public void test_strtoint_onenumber()
+	{
+		int[] arr = new int[1];
+		arr[0] = 1;
+		assertArrayEquals(arr,Calculator.strtoint("1"));
+	}
+
 }
