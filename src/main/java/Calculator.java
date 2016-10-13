b2e2a7c644a7e383ec516f2d8cd56825251e77c2
hutton85is <hutton85is@yahoo.com>
2016-10-13 18:41:25 +0000

    Created skeleton for project and the first test for string calculator function add can take empty string and return zero

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+package String_Calculator;
+
+import java.util.*;
+
+public class Calculator
+{
+	public static int add(String text){
+		return 0;
+	}
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+package String_Calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+	//Test input of an empty string
+	@Test
+	public void testEmptyString()
+	{
+		assertEquals(0, Calculator.add(""));
+	}
+}
